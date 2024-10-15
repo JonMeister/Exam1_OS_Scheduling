aaa0637832deca20789a698e002e37bf194835d1
JonMeister <jonathanaris99@gmail.com>
2024-10-15 02:07:52 -0500

    ready to run in linux

---
+++ CMakeFiles/Exam1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jonmeister/1_SemesterIV/Operating_systems/Exam1/Exam1_OS_Scheduling/Algorithm.cpp" "CMakeFiles/Exam1.dir/Algorithm.cpp.o" "gcc" "CMakeFiles/Exam1.dir/Algorithm.cpp.o.d"
+  "/home/jonmeister/1_SemesterIV/Operating_systems/Exam1/Exam1_OS_Scheduling/Process.cpp" "CMakeFiles/Exam1.dir/Process.cpp.o" "gcc" "CMakeFiles/Exam1.dir/Process.cpp.o.d"
+  "/home/jonmeister/1_SemesterIV/Operating_systems/Exam1/Exam1_OS_Scheduling/main.cpp" "CMakeFiles/Exam1.dir/main.cpp.o" "gcc" "CMakeFiles/Exam1.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Exam1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exam1.dir/Algorithm.cpp.o"
+  "CMakeFiles/Exam1.dir/Algorithm.cpp.o.d"
+  "CMakeFiles/Exam1.dir/Process.cpp.o"
+  "CMakeFiles/Exam1.dir/Process.cpp.o.d"
+  "CMakeFiles/Exam1.dir/main.cpp.o"
+  "CMakeFiles/Exam1.dir/main.cpp.o.d"
+  "Exam1"
+  "Exam1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exam1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
